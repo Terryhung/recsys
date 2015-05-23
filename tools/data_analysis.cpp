88d69717d4b7e912a33cc91796878cf773aa361f
duckingod <kingoduck@gmail.com>
2015-05-24 02:43:11 +0800

    Add note #11, add new feature to data_analysis.

--- tools/data_analysis.cpp
+++ tools/data_analysis.cpp
@@ -8,18 +8,23 @@
 
 #include <vector>
 
+#include "Session.cpp"
+
 using namespace std;
 
-const int BUF_SIZE = 10000;
 char s[BUF_SIZE];
 int main() {
-    FILE *item_id_f;
+    FILE *item_id_f, *click_f, *buy_f;
     puts("Should execute this program in \'recsys\' folder.");
     item_id_f = fopen("item_id_table.txt", "rb");
+    click_f = fopen("out-click-all.dat", "rb");
+    buy_f = fopen("out-buy-all.dat", "rb");
     if (!item_id_f) { puts("item_id_table.txt open error."); return 0; }
+    if (!click_f) { puts("out-click-all.dat open error."); return 0; }
+    if (!buy_f) { puts("out-buy-all.dat open error."); return 0; }
     int pos, id, num;
     vector<int> ary;
-    fseek(item_id_f, 0, SEEK_SET);
+
     while (!feof(item_id_f)) {
         if (fscanf(item_id_f, "%d:%d:%d", &pos, &id, &num)<3) break;
         ary.push_back(num);
@@ -35,9 +40,48 @@ int main() {
             acc += ary[p];
             p++;
         }
-        printf("%5d item got more than %8d clicks. (total %9d clicks) (if it\'s thres, purity:%f)\n", (ary.size()-p-1), ary[p], acc, (ary.size()*1.0/(ary.size()-p-1)) * (total-acc)*1.0/total);
+        printf("%5lu item got more than %8d clicks. (total %9d clicks) (if it\'s thres, purity:%f)\n", (ary.size()-p-1), ary[p], acc, (ary.size()*1.0/(ary.size()-p-1)) * (total-acc)*1.0/total);
+    }
+    fclose(item_id_f);
+
+    vector<pair<int, int> > se_ary;
+    vector<int> buy_ary;
+    Session se;
+    se = Session(Session::CLICK);
+    while (se.read_binary(click_f).id()!=0) {
+        se_ary.push_back(make_pair(se.id(), se.len()));
+    }
+    se = Session(Session::BUY);
+    fclose(click_f);
+    while (se.read_binary(buy_f).id()!=0) {
+        buy_ary.push_back(se.id());
+    }
+    fclose(buy_f);
+    sort(se_ary.begin(), se_ary.end());
+    sort(buy_ary.begin(), buy_ary.end());
+    buy_ary.push_back(0);
+    p = 0;
+    for (int i=0; i<se_ary.size(); i++) {
+        while (p<buy_ary.size()-1 && buy_ary[p]<se_ary[i].first)
+            p++;
+        if (buy_ary[p]==se_ary[i].first)
+            se_ary[i].first = -se_ary[i].first;
+        std::swap(se_ary[i].first, se_ary[i].second);
+    }
+    sort(se_ary.begin(), se_ary.end());
+    // se_ary = [(len, +-id) ... ]
+    p = 1;
+    acc = 0;
+    for (float i=2; p!=ary.size()-1; i*=1.5) {
+        while (p<se_ary.size()-1 && se_ary[p].first<i) {
+            acc += (se_ary[p].second<0);
+            p++;
+        }
+        printf("%d sessions with #click<%.0f have %d buys. (ratio:%.3f)\n", p, i, acc, acc*1.0/p);
     }
     
+    
+
 
 	return 0;
 }
