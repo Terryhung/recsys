886df2f9d55347ad36425af6630a3ba45ca5e802
duckingod <kingoduck@gmail.com>
2015-05-23 19:58:03 +0800

    Add -s 2 to gendata

--- gendata_train_predict_refine_data.sh
+++ gendata_train_predict_refine_data.sh
@@ -11,7 +11,7 @@ set -v
 ./tools/data_refiner out-click-all.dat $dataprefix.dat $1 $2 
 python tools/make_liblinear_bits.py $dataprefix.dat out-buy-all.dat $liblinearprefix.dat
 tools/make_partial_valid_data $liblinearprefix.dat
-./train $liblinearprefix-partial.dat
+./train -s 2 $liblinearprefix-partial.dat
 ./predict $liblinearprefix-valid.dat $liblinearprefix-partial.dat.model res.txt
 ./tools/result_01_grid $liblinearprefix-valid.dat res.txt
 
