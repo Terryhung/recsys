2a80d81fe32bf29fdc6165daa3ce5c530ff2748a
terryhung <terryhung28@cml5.csie.ntu.edu.tw>
2015-05-26 01:47:11 +0800

    Change function structure of generating feat in make_liblinear_bits. temporarly untested version.

--- tools/make_liblinear_bits.py
+++ tools/make_liblinear_bits.py
@@ -22,13 +22,6 @@ def item_feature_maker_month_cate(item):
         s += str(27*item.item_id + 12+item.cate) + ":1 "
     return s
 
-def item_feature_maker_cate(item):
-    if item.cate>=14:
-        s = str(15*item.item_id + 14) + ":1 "
-    else:
-        s = str(15*item.item_id + item.cate) + ":1 "
-    return s
-
 def item_feature_maker_spec_offer(item):
     if item.cate==13:
         s = str(2*item.item_id + 1) + ":1 "
@@ -36,17 +29,74 @@ def item_feature_maker_spec_offer(item):
         s = str(2*item.item_id + 0) + ":1 "
     return s
 
-def write(y, se, f, item_feature_maker):
-    s = str(y)+" "
-    data_tool.session_sort_info_by_item_id(se)
-    lastitem = None
-    for item in se.info:
-        if lastitem!=None:
+
+def item_feature_maker_numbeclick(item, se_info):
+    nb = se_info['item_be_click'][item.item_id]
+    if nb<=3:
+        return (nb-1, 4)
+    else:
+        return (3, 4)
+
+def item_feature_maker_cate(item, se_info):
+    if item.cate>=14:
+        return ([14], 15)
+    else:
+        return ([item.cate], 15)
+
+
+
+def preprocessor_dict(se):
+    res_dict = {}
+    cnt = 1
+    res_dict['item_be_click'] = {}
+    se.info.append(ClickInfo())
+    for i in xrange(len(se.info)-1):
+        if se.info[i].item_id!=se.info[i+1].item_id:
+            res_dict['item_be_click'][se.info[i].item_id] = cnt
+            cnt = 1
+    se.info = se.info[:-1]
+    return res_dict
+
+def session_feature_maker_numclick(se):
+    res = []
+    for i in range(0, 5):
+        if 2**(2*i-2)<se.num_click and se.num_click<2**(2*i):
+            res.append(i)
+    return (s, 5)
+
+def get_feat_string(feat_list, feat_cnt):
+    s = ""
+    for f in feat_list:
+        s += str(f+feat_cnt) + ":1 "
+    return s
+
+def get_data_feature_maker(preprocessor, session_feature_maker, item_feature_makers):
+    def data_feature_maker(y, se):
+        data_tool.session_sort_info_by_item_id(se)
+        se_info = preprocessor(se)
+        s = str(y) + " "
+        feat_cnt = 1
+        (feat_list, res_featcnt) = session_feature_maker(se)
+        s += get_feat_string(feat_list, res_feat_cnt)
+        feat_cnt += res_featcnt
+        lastitem = ClickInfo()
+        for item in se.info:
             if item.item_id==lastitem.item_id:
                 continue
-        lastitem = item
-        s += item_feature_maker(item)
-    print >>f, s[:-1]
+            else:
+                lastitem = item
+            if not type(item_feature_makers) is list:
+                item_feature_makers = [item_feature_makers]
+            for ifm in item_feature_maker:
+                (feat_list, res_feat_cnt) = ifm(item, se_info)
+                s += get_feat_string(feat_list, res_feat_cnt)
+                feat_cnt += res_featcnt
+        return s
+    return session_feature_maker
+
+
+def write(y, se, f, data_feature_maker):
+    print >>f, data_feature_maker(y, se)[:-1]
     
 
 def main():
@@ -70,20 +120,24 @@ def main():
     print "Start to write to "+sys.argv[3]
     nw_buy = 0
     i = 0
-    item_feature_maker = item_feature_maker_cate
+    data_feature_maker = get_data_feature_maker(
+                                                preprocessor_dict, 
+                                                session_feature_maker_numclick, 
+                                                [item_feature_maker_numbeclick, item_feature_maker_cate])
     with open(sys.argv[1], "r") as f, open(sys.argv[3], "w") as out:
-        s = Session("click")
+        se = Session("click")
         while s.read_binary(f).session_id!=0:
             data_tool.session_reduce_id_brand(s, item_id_map, brand_map)
             if buy_id.get(s.session_id)!=None:
-                write(1, s, out, item_feature_maker)
+                y = 1
             else:
-                write(0, s, out, item_feature_maker)
+                y = 0
+            write(y, se, out, data_feature_maker)
             i += 1
             if i%100000==0:
                 print str(i) + " data ok."
                 print "    now session_id="+str(s.session_id)
-            s = Session("click")
+            se = Session("click")
     print "Done"
     
 
